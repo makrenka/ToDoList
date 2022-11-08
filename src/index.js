c41f7f77cb8970ce17eb39606cbfddf55b030078
makrenka <smokrenko79@gmail.com>
2022-11-08 20:14:33 +0300

    add methods in Database

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 import "./main.scss";
-import "./components/Button/Button";
 
-const root = document.querySelector('#root');
 
-root.innerHTML = `<my-button></my-button>`;
---
+++ src/services/database/Database.js
@@ -0,0 +1,50 @@
+import { initializeApp } from "firebase/app";
+import { addDoc, collection, deleteDoc, doc, getDocs, getFireStore, updateDoc } from 'firebase/firestore';
+import { API_KEY } from "../../constants/envValues";
+
+export class Database {
+    constructor() {
+        const firebaseConfig = {
+            apiKey: API_KEY,
+            authDomain: "it-todo-list-bfd1e.firebaseapp.com",
+            projectId: "it-todo-list-bfd1e",
+            storageBucket: "it-todo-list-bfd1e.appspot.com",
+            messagingSenderId: "914302045923",
+            appId: "1:914302045923:web:9d6c5dd75043cf2cf0ffd6",
+            measurementId: "G-8RMV8MQCDR"
+        };
+
+        const app = initializeApp(firebaseConfig);
+        this._database = getFireStore(app);
+    }
+
+    create(collectionKey, body) {
+        const collectionRef = collection(this._database, collectionKey);
+        return addDoc(collectionRef, body);
+    }
+
+    read(collectionKey) {
+        const collectionRef = collection(this._database, collectionKey);
+        return getDocs(collectionRef).then((documents) => {
+            return documents.docs.map((doc) => ({...doc.data(), id: doc.id}))
+        });
+    }
+
+    update(collectionKey, id, body) {
+        const document = doc(this._database, collectionKey, id);
+        return updateDoc(document, body);
+    }
+
+    delete(collectionKey, id) {
+        const document = doc(this._database, collectionKey, id); 
+        return deleteDoc(document);
+    }
+
+    static getInstance() {
+        if (!Database.instance) {
+            Database.instance = new Database();
+        }
+
+        return Database.instance;
+    }
+}
