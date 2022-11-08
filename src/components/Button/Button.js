02979e881a1e93b0b886e7663d0b729797e4f54e
makrenka <smokrenko79@gmail.com>
2022-11-08 18:40:18 +0300

    add webpack

---
+++ src/components/Button/Button.js
@@ -0,0 +1,50 @@
+import { Component } from "../../core";
+import './button.scss';
+
+export class Button extends Component {
+    constructor() {
+        super();
+        this.state = {
+            count: 1,
+        }
+    }
+
+    plus() {
+        this.setState((state) => {
+            return {
+                ...state,
+                count: state.count < 10 ? state.count += 1 : state.count,
+            }
+        });
+    }
+
+    minus() {
+        this.setState((state) => {
+            return {
+                ...state,
+                count: state.count > 0 ? state.count -= 1 : state.count,
+            }
+        });
+    }
+
+    registerEvents() {
+        this.addEventListener('click', (evt) => {
+            if (evt.target.closest('.plus')) {
+                this.plus();
+            }
+            if (evt.target.closest('.minus')) {
+                this.minus();
+            }
+        })
+    }
+
+    render() {
+        return `
+            <button class="plus">+</button>
+            <span>${this.state.count}</span>
+            <button class="minus">-</button>
+        `
+    }
+}
+
+customElements.define('my-button', Button);
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./main.scss";
+import "./components/Button/Button";
+
+const root = document.querySelector('#root');
+
+root.innerHTML = `<my-button></my-button>`;
