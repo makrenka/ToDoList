d7d2ea54180c549b57d1e3742fbc1304ab84fdcf
makrenka <smokrenko79@gmail.com>
2022-11-10 16:14:08 +0300

    add spinner-border

--- src/App.js
+++ src/App.js
@@ -36,27 +36,31 @@ export class App extends Component {
 
     render() {
         return `
-            
+        <div class="d-flex justify-content-center position-absolute">
+            <div class="spinner-border" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
         <div class='container mt-5'>
             <div class="input-group mb-3">
-            <input value='${this.state.value}' type="text" class="form-control" placeholder="Add a new task" aria-label="Recipient's username" aria-describedby="button-addon2">
-            <my-button content="save" classname="btn btn-outline-primary"></my-button>
+                <input value='${this.state.value}' type="text" class="form-control" placeholder="Add a new task" aria-label="Recipient's username" aria-describedby="button-addon2">
+                <my-button content="save" classname="btn btn-outline-primary"></my-button>
             </div>
             <ul class="list-group">
-            <li class="list-group-item">
-                <div class="form-check d-flex justify-content-between align-items-center">
-                <div>
-                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                    <label class="form-check-label" for="flexCheckDefault">
-                        Default checkbox
-                    </label>
+                <li class="list-group-item">
+                    <div class="form-check d-flex justify-content-between align-items-center">
+                    <div>
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                        <label class="form-check-label" for="flexCheckDefault">
+                            Default checkbox
+                        </label>
+                        </div>
+                        <div class='d-flex'>
+                        <my-button content="Delete" classname="btn btn-danger btn-sm"></my-button>
+                        <my-button content="Update" classname="btn btn-primary btn-sm"></my-button>
+                        </div>
                     </div>
-                    <div class='d-flex'>
-                    <my-button content="Delete" classname="btn btn-danger btn-sm"></my-button>
-                    <my-button content="Update" classname="btn btn-primary btn-sm"></my-button>
-                    </div>
-                </div>
-            </li>
+                </li>
             </ul>
         </div>
         `
