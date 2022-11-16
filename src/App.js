c8412611c088dbe6f09edafa1b705eccdd3dee61
makrenka <smokrenko79@gmail.com>
2022-11-16 18:02:46 +0300

    delete methods in App

--- src/App.js
+++ src/App.js
@@ -5,44 +5,16 @@ import { todoList } from './services/todoList/TodoList';
 
 export class App extends Component {
 
-    constructor() {
-        super();
-        this.state = {
-            isLoading: false,
-            value: '',
-        }
-    }
-
-    registerEvents() {
-        this.addEventListener('change', (evt) => {
-            if (evt.target.closest('.form-control')) {
-                this.setState((state) => {
-                    return {
-                        ...state,
-                        value: evt.target.value,
-                    }
-                })
-            }
-        });
-
-        window.addEventListener("save-task", () => {            
-            this.setState((state) => ({ ...state, isLoading: true }));
-            todoList.createTask({ title: this.state.value }).finally(() => {
-                this.setState((state) => ({ ...state, isLoading: false }))
-            })
-        })
-    }
-
-    
 
     render() {
         return `
         
         <div class='container mt-5'>
             <div class="input-group mb-3">
-                <my-input type="text" placeholder="Add a new task"><my-input>
+                <my-input type="text" placeholder="Add a new task"></my-input>
                 <my-button eventtype="save-task" content="Save" classname="btn btn-outline-primary"></my-button>
             </div>
+
             <ul class="list-group">
                 <li class="list-group-item">
                     <div class="form-check d-flex justify-content-between align-items-center">
@@ -53,8 +25,8 @@ export class App extends Component {
                         </label>
                         </div>
                         <div class='d-flex'>
-                        <my-button content="Delete" classname="btn btn-danger btn-sm"></my-button>
-                        <my-button content="Update" classname="btn btn-primary btn-sm"></my-button>
+                            <my-button content="Delete" classname="btn btn-danger btn-sm"></my-button>
+                            <my-button content="Update" classname="btn btn-primary btn-sm"></my-button>
                         </div>
                     </div>
                 </li>
